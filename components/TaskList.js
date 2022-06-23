556ea47d34218c298a2242a80673c49d614f26dc
Jofleming <jordano.fleming@gmail.com>
2022-06-22 18:16:52 -0700

    changed button to checkbox and fixed render logic. Footer now renders incomplete task amount only. Incomplete and complete tasks now render separately

--- components/TaskList.js
+++ components/TaskList.js
@@ -1,7 +1,7 @@
 
 import useResource from '../hooks/useResource';
 import Accordion from "../components/Accordion";
-export default function TaskList({ taskData}) {
+export default function TaskList({ taskData }) {
     const styles = {
         fontFamily: "Roboto"
       };
@@ -19,29 +19,53 @@ export default function TaskList({ taskData}) {
         // e.target.reset();
     }
 
-
+    let notCompleted = taskData.filter(item => (item.completed == false));
+    let isCompleted = taskData.filter(item => (item.completed == true));
     return (
+        <>
         <div style={styles}>
-        {taskData.map((task, idx) => {
+        {isCompleted.map((task, idx) => {
             return(
-            <>
-
-            <Accordion
-            taskData={task} 
-            title={task.title} 
-            content={task.description}
-            due={task.due} 
-            com={task.completed}
-            key={idx} 
-            deleteTask={deleteResource} 
-            updateResource={handlePut}
+                
+                <>
+                <Accordion
+                taskData={task} 
+                title={task.title} 
+                content={task.description}
+                due={task.due} 
+                com={task.completed}
+                key={idx} 
+                deleteTask={deleteResource} 
+                updateResource={handlePut}
             // handlePut={handlePut}
-            id={task.id}
-            />
-            </>
+                id={task.id}
+                />
+                </>
             )
         })}
         </div>
+        <div style={styles}>
+        {notCompleted.map((task, idx) => {
+            return(
+                
+                <>
+                <Accordion
+                taskData={task} 
+                title={task.title} 
+                content={task.description}
+                due={task.due} 
+                com={task.completed}
+                key={idx} 
+                deleteTask={deleteResource} 
+                updateResource={handlePut}
+            // handlePut={handlePut}
+                id={task.id}
+                />
+                </>
+            )
+        })}
+        </div>
+        </>
     )
 
    
